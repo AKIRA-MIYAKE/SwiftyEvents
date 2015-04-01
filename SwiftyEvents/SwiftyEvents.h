f7af185464452c7add2d49d071ad4357272630e0
AKIRA-MIAYEK <akira.aktk.miyake@gmail.com>
2015-04-01 20:44:55 +0900

    Initial Commit

---
+++ SwiftyEvents/SwiftyEvents.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyEvents.h
+//  SwiftyEvents
+//
+//  Created by MiyakeAkira on 2015/04/01.
+//  Copyright (c) 2015年 Miyake Akira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyEvents.
+FOUNDATION_EXPORT double SwiftyEventsVersionNumber;
+
+//! Project version string for SwiftyEvents.
+FOUNDATION_EXPORT const unsigned char SwiftyEventsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyEvents/PublicHeader.h>
+
+
